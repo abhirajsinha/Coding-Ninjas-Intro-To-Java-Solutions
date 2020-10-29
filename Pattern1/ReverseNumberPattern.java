9a836255860010c851377997a7572d5b0f3c95e2
Abhiraj Sinha <56290450+abhirajsinha@users.noreply.github.com>
2020-10-29 12:14:04 +0530

    Create ReverseNumberPattern.java

---
+++ Pattern1/ReverseNumberPattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {
+			Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i=1;
+		while(i<=n) {
+			int j=i;
+			while(j>=1) {
+				System.out.print(j);
+				j--;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+
+}
