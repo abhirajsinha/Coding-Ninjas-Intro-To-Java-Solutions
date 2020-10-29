e6bfb42beba93b603edad5fe62d47156a6db3ce6
Abhiraj Sinha <56290450+abhirajsinha@users.noreply.github.com>
2020-10-29 12:49:51 +0530

    Create Remove Consecutive Duplicates.java

---
+++ Strings and 2D Arrays/Remove Consecutive Duplicates.java
@@ -0,0 +1,14 @@
+public class solution {
+
+	public static String removeConsecutiveDuplicates(String str) {
+String x="";
+		x=x+str.charAt(0);
+		for(int i=1;i<str.length();i++) {
+			if(str.charAt(i)!=x.charAt(x.length()-1)) {
+				x=x+str.charAt(i);
+			}
+		}
+		return x;
+        
+	}
+}
