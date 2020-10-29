df09192031c54ad2be104e875951523ba78ef1bc
Abhiraj Sinha <56290450+abhirajsinha@users.noreply.github.com>
2020-10-29 12:15:05 +0530

    Create CharacterPattern.java

---
+++ Pattern1/CharacterPattern.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+			Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i=1;
+		
+		while(i<=n)
+		{
+			int j=1;
+			char start=(char)('A'+i-1);
+			while(j<=i) {
+				System.out.print(start);
+				start=(char)(start+1);
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+		
+	}
+
+}
