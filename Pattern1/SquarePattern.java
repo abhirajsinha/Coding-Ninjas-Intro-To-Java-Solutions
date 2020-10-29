e1ff702a8e097e85598e1a97064181bb0b240e0f
Abhiraj Sinha <56290450+abhirajsinha@users.noreply.github.com>
2020-10-29 12:11:12 +0530

    Create SquarePattern.java

---
+++ Pattern1/SquarePattern.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {		
+		Scanner s=new Scanner(System.in);
+		int i=1;
+	    int n=s.nextInt();
+	while(i<=n)
+	{
+		int j=1;
+	while(j<=n)
+	{
+		System.out.print(n);
+		j++;
+	}
+	System.out.println();
+	i++;
+	}
+	}
+
+}
