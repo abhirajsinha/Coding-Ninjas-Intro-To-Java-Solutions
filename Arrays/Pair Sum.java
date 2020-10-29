3b1be87bc67d22d689758256a9eb0e4e02a6d8e4
Abhiraj Sinha <56290450+abhirajsinha@users.noreply.github.com>
2020-10-29 12:36:24 +0530

    Create Pair Sum.java

---
+++ Arrays/Pair Sum.java
@@ -0,0 +1,15 @@
+public class Solution {  
+
+    public static int pairSum(int arr[], int x) {
+  		int pair=0;
+        
+    	for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]+arr[j]==x){
+                    pair=pair+1;
+                }
+            }
+        }
+        return pair;
+    }
+}
